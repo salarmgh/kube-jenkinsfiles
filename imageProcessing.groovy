c65d58e386045c181c140cd054cf783f23eb483f
Salar Moghaddam <salarmgh@gmail.com>
2022-06-17 01:16:22 +0430

    update

--- imageProcessing.groovy
+++ imageProcessing.groovy
@@ -18,11 +18,17 @@ spec:
     stage('Build') {
       steps {
         script {
+          checkout([
+            $class: "GitSCM",
+            branches: [[name: "*/main"]],
+            userRemoteConfigs: [[url: "https://github.com/salarmgh/kube-app.git"]]
+          ])
           container('dind') {
             script {
-              sh """
-                echo 'Build ...'
-              """
+                  sh """
+                  docker build -t test .
+                  """
+              }
             }
           }
         }
