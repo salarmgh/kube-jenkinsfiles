7d3e2a97044bf7d3813ef1dde9b4007c2a1a3099
Salar Moghaddam <salarmgh@gmail.com>
2022-06-17 10:26:24 +0430

    Update prod

--- imageProcessingProd.groovy
+++ imageProcessingProd.groovy
@@ -9,8 +9,9 @@ spec:
     image: docker:stable-dind
     imagePullPolicy: IfNotPresent
     command:
-    - --insecure-registry='registry.anisa.lab'
-    - --registry-mirror='http://registry.anisa.lab'
+    - dockerd-entrypoint.sh
+    - '--insecure-registry=registry.anisa.lab'
+    - '--registry-mirror=http://registry.anisa.lab'
     securityContext:
       privileged: true
 """
